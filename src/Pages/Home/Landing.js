c1e1c3ee24aa4ad1d2526933ae457f17d8a2ef4c
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-15 17:14:50 +0600

    Get Free Estimate section done

--- src/Pages/Home/Landing.js
+++ src/Pages/Home/Landing.js
@@ -3,6 +3,8 @@ import image from '../../assets/images/bucketgirl.png'
 
 const Landing = () => {
   return (
+    
+    <div className='relative'>
     <div class="hero h-screen lg:h-[60vh] mt-16 bg-accent">
       <div class="hero-content flex-col lg:flex-row">
         <div>
@@ -24,6 +26,55 @@ const Landing = () => {
         </div>
       </div>
     </div>
+
+    <div className='rounded-2xl mx-auto mt-[-50px] relative z-20 bg-base-200 shadow-lg p-10 w-5/6'>
+        <h1 className='text-2xl mb-5 text-primary'>Get Free Estimate</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 '>
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+          <input
+            type='text'
+            placeholder='Type here'
+            class='input input-bordered w-full'
+          />
+        </div>
+        <button className='btn btn-primary mt-5'>Get a Quote</button>
+      </div>
+      </div>
+
   );
 };
 
