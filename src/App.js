4d34b04398dc0a33f714a719c98e6514ae0e6db0
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-15 16:39:17 +0600

    navbar menu done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+import { Component } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Services from "./Pages/Services";
+import publicRoute from "./routes/publicRoutes";
+
 
 function App() {
   return (
-  
-      <Navbar>
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/services" element={<Services/>}></Route>
-          <Route path="/contact" element={<Contact/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-        </Routes>
-      </Navbar>
-   
+    <Navbar>
+      <Routes>
+        {publicRoute.map(({path, Component} , index) => (
+          <Route key={index} path={path} element={<Component />} />
+        ))}
+      </Routes>
+    </Navbar>
   );
 }
 
